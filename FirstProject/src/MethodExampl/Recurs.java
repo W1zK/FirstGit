aed98f3bc54b86c4f32b8421f766ab42631050f4
Aleksey <rezorqqq5@gmail.com>
2019-07-04 13:45:10 +0300

    fibonacci

--- FirstProject/src/MethodExampl/Recurs.java
+++ FirstProject/src/MethodExampl/Recurs.java
@@ -1,17 +1,46 @@
 package MethodExampl;
 
 public class Recurs {
-    static String printNumbers(int n) {
-        if (n <= 0) return "";
+    //    static String printNumbers(int n) {
+//        if (n <= 0) return "";
+//
+//        return printNumbers(n - 1) + " " + n;
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        String str = printNumbers(10000);
+//        System.out.println(str);
+//
+//    }
+//    static int fibonaci(int n) {
+//        if (n <= 0) return 0;
+//        int value = fibonaci(n - 1);
+//        return value + n;
+//
+//
+//
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        for(int i = 0; i < 10; i++) {
+//            System.out.println("fib" + fibonaci(i));
+//        }
+//    }
+    static int fibonacci(int n) {
+        if (n < 2) return n;
 
-        return printNumbers(n - 1) + "" + n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+
 
     }
 
     public static void main(String[] args) {
-        String str = printNumbers(5);
-        System.out.println(str);
+        for (int i = 0; i < 20; i++) {
+            System.out.println("fib" + " " + fibonacci(i));
+        }
+
 
     }
-
 }
