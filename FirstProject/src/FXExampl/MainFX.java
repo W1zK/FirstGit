87d88ba768d8d710b28e32c98d77a38c984271d3
Aleksey <rezorqqq5@gmail.com>
2019-07-08 17:28:07 +0300

    workFX

--- FirstProject/src/FXExampl/MainFX.java
+++ FirstProject/src/FXExampl/MainFX.java
@@ -2,6 +2,8 @@ package FXExampl;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -30,21 +32,43 @@ public class MainFX extends Application {
     }
 
     void uiSetup(Pane root) {
+//---------------------TEXT------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------
         Text text = new Text("HELLO WORLD");
         text.setTranslateX(1000);
         text.setTranslateY(500);
-//        text.setTranslateZ(150);
+ //------------------TEXTFIELD------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------
+        TextField textIn = new TextField("100");
+        textIn.setTranslateX(300);
+        textIn.setTranslateY(100);
+
+
+
+//----------------Button---------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------
 
         Button button = new Button("Добавить число");
 
         button.setTranslateX(50);
         button.setTranslateY(100);
 
-        root.getChildren().addAll(button, text);
+
+        button.setOnMouseClicked((event) -> {
+            String number = textIn.getText();
+            String oldText = text.getText();
+            text.setText(oldText + "\n" + number);
+
+//            root.getChildren().addAll(text2);
+        });
+
+        root.getChildren().addAll(button, text, textIn);
+
+
 
 
     }
-
+ 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
