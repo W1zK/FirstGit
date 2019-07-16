621366d293fdd358fbd8b5bbe2f54ed2d811a1ad
Aleksey <rezorqqq5@gmail.com>
2019-07-16 20:05:49 +0300

    123

---
+++ FirstProject/src/main/java/lesson8/ThreadMethodExample.java
@@ -0,0 +1,44 @@
+package lesson8;
+
+public class ThreadMethodExample {
+    static Thread  showTimer(int timer) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int seconds = 0;
+                while (seconds < timer) {
+                    try {
+                        Thread.sleep(1000);
+                        System.out.println(Thread.currentThread().getName() + "   " + ++seconds);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+
+                }
+            }
+        });
+        thread.start();
+        return thread;
+
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        showTimer(3).join();
+        showTimer(5).join();
+        showTimer(1).join();
+
+
+        for (int i = 0; i < 3; i++) {
+            int fi = i;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("i = " + fi);
+
+                }
+            }).start();
+        }
+    }
+}
